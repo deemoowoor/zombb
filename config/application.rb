08879c67985cd4d0c19311e7898a138f6e7ecf49
Andrei Sosnin <andrei.sosnin@gmail.com>
2014-11-11 14:44:23 +0200

    Add bower components to make heroku happy

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,21 @@ module Zombb
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+
+    # via https://github.com/sstephenson/sprockets/issues/347#issuecomment-25543201
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+            '.html', '.erb', '.haml',                 # Templates
+            '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+            '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+    end)
+    # config/environments/production.rb
+    config.assets.compile = true
   end
 end
