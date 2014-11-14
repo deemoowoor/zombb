c863a172be5daf865da8482e7fb9eeb4c41d0d86
Andrei Sosnin <andrei.sosnin@gmail.com>
2014-11-14 10:31:11 +0200

    Migrated User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111112846) do
+ActiveRecord::Schema.define(version: 20141114081400) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
